c2f592dff063183c5b96bdc001551f604ebac4fe
kobaken <kentafly88@gmail.com>
2025-05-24 10:39:40 +0900

    Fixed type error

--- src/rsc-renderer.ts
+++ src/rsc-renderer.ts
@@ -5,6 +5,12 @@ import * as RSC from "@hiogawa/vite-rsc/extra/rsc";
 
 export interface Props {}
 
+declare module 'hono' {
+  interface ContextRenderer {
+    (children: React.ReactElement, props?: Props): Response | Promise<Response>
+  }
+}
+
 type RendererOptions = {
   nonce?: string
   unsafeEval?: boolean
