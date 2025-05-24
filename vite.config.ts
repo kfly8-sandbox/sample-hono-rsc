83ef6b7a6e11ff85b10b6e70853f38a488cda451
kobaken <kentafly88@gmail.com>
2025-05-24 21:55:03 +0900

    refactor vite.config.ts

--- vite.config.ts
+++ vite.config.ts
@@ -1,4 +1,5 @@
 import { defineConfig } from 'vite'
+import type { Plugin } from 'vite'
 import react from "@vitejs/plugin-react"
 import rsc from "@hiogawa/vite-rsc/plugin";
 
@@ -12,51 +13,54 @@ export default defineConfig({
         ssr: "@hiogawa/vite-rsc/extra/ssr",
       }
     }),
-    {
-      name: "cf-build",
-      enforce: "post",
-      apply: () => !!process.env.CF_BUILD,
-      configEnvironment() {
-        return {
-          keepProcessEnv: false,
-          define: {
-            "process.env.NO_CSP": "false",
-          },
-          resolve: {
-            noExternal: true,
-          },
-        };
-      },
-      generateBundle() {
-        if (this.environment.name === "rsc") {
-          this.emitFile({
-            type: "asset",
-            fileName: "cloudflare.js",
-            source: `\
-  import handler from './index.js';
-  export default { fetch: handler };
-  `,
-          });
-        }
-        if (this.environment.name === "client") {
-          // https://developers.cloudflare.com/workers/static-assets/headers/#custom-headers
-          this.emitFile({
-            type: "asset",
-            fileName: "_headers",
-            source: `\
-  /favicon.ico
-  Cache-Control: public, max-age=3600, s-maxage=3600
-  /assets/*
-  Cache-Control: public, max-age=31536000, immutable
-  `,
-          });
-        }
-      }
-    }
+    createCloudflarePlugin(),
   ],
   build: {
     minify: true,
   },
 })
 
+function createCloudflarePlugin(): Plugin {
+  return {
+    name: "cf-build",
+    enforce: "post",
+    apply: () => !!process.env.CF_BUILD,
+    configEnvironment() {
+      return {
+        keepProcessEnv: false,
+        define: {
+          "process.env.NO_CSP": "false",
+        },
+        resolve: {
+          noExternal: true,
+        },
+      };
+    },
+    generateBundle() {
+      if (this.environment.name === "rsc") {
+        this.emitFile({
+          type: "asset",
+          fileName: "cloudflare.js",
+          source: `\
+import handler from './index.js';
+export default { fetch: handler };
+`,
+        });
+      }
+      if (this.environment.name === "client") {
+        // https://developers.cloudflare.com/workers/static-assets/headers/#custom-headers
+        this.emitFile({
+          type: "asset",
+          fileName: "_headers",
+          source: `\
+/favicon.ico
+Cache-Control: public, max-age=3600, s-maxage=3600
+/assets/*
+Cache-Control: public, max-age=31536000, immutable
+`,
+        });
+      }
+    }
+  };
+}
 
